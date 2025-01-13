c45d22e74fbc2edea2c7df30df8feafefacb83a0
Ben Sayed <Ahmed8Sayed1010@gmail.com>
2025-01-13 18:05:42 +0200

    creat a page login

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PageErorr500 from './Page Erorr/Page500/pageErorr500';
 import Erorr404 from './Page Erorr/page404/Erorr404';
 import Countact from './page-index/Contaut us/ContautUs';
  import Header from './Component/Header/Header';
+import Login from './LoginPage/login/login';
 
 function App() {
  
@@ -17,7 +18,8 @@ function App() {
       {/* <WaitingPage/> */}
       {/* <PageErorr500/> */}
       {/* <Erorr404/> */}
-      <Home/>
+      {/* <Home/> */}
+      <Login/>
   
     </>
   );
