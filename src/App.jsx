9dc126ef76109268e38c514018a4cc5f13d43bbf
Ben Sayed <Ahmed8Sayed1010@gmail.com>
2025-01-14 13:58:02 +0200

    عمل صفحه المنيو ريسبونسيف

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import Home from './page-index/Home/Home';
-import WaitingPage from './Handel Page/Waiting page/waitingPage';
-import PageErorr500 from './Page Erorr/Page500/pageErorr500';
-import Erorr404 from './Page Erorr/page404/Erorr404';
-import Countact from './page-index/Contaut us/ContautUs';
- import Header from './Component/Header/Header';
-import Login from './LoginPage/login/login';
-import SignUp from './LoginPage/Sign up/SignUp';
+import Home from "./page-index/Home/Home";
+import WaitingPage from "./Handel Page/Waiting page/waitingPage";
+import PageErorr500 from "./Page Erorr/Page500/pageErorr500";
+import Erorr404 from "./Page Erorr/page404/Erorr404";
+import Countact from "./page-index/Contaut us/ContautUs";
+import Header from "./Component/Header/Header";
+import Login from "./LoginPage/login/login";
+import SignUp from "./LoginPage/Sign up/SignUp";
+import ExploreMenu from "./page-index/MenuPage/ExploreMenu";
 
 function App() {
- 
   return (
     <>
-      <div> 
-       <Header/>
-        
+      <div>
+        <Header />
       </div>
       {/* <Countact /> */}
       {/* <WaitingPage/> */}
@@ -21,9 +20,10 @@ function App() {
       {/* <Erorr404/> */}
       {/* <Home/> */}
       {/* <Login/> */}
-  <SignUp/>
+      {/* <SignUp/> */}
+      <ExploreMenu />
     </>
   );
 }
 
-export default App
+export default App;
---
+++ src/page-index/MenuPage/ExploreMenu.jsx
@@ -0,0 +1,198 @@
+import React from "react";
+import "./ExploreMenu.css";
+const ExploreMenu = () => {
+  return (
+    <div>
+      <div className="food-selection0">
+        <h2>Main Courses</h2>
+      </div>
+      <div className="cardss">
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Wagyu Steak"
+          title="Wagyu Steak"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="78$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Lobster Roll"
+          title="Lobster Roll"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="28$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="98$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />{" "}
+        <Card
+          imgSrc="src/page-index/MenuPage/Rectangle 1153 (1).svg"
+          altText="Cheesecake"
+          title="Cheesecake"
+          description="250g of lean steak with sous smashed potato or rice"
+          price="110$"
+          heartImg="src/page-index/MenuPage/solar_heart-bold (2).svg"
+        />
+      </div>
+      {/* Repeat the card group as needed */}
+    </div>
+  );
+};
+
+// eslint-disable-next-line react/prop-types
+const Card = ({ imgSrc, altText, title, description, price, heartImg }) => {
+  return (
+    <section className="ExploreMenuCard">
+      <div className="cardd">
+        <img src={imgSrc} alt={altText} />
+        <div className="content">
+          <div className="text">
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <div className="stars">&#9733; &#9733; &#9733; &#9733; &#9733;</div>
+          </div>
+          <div className="price">{price}</div>
+          <div className="heart">
+            <img src={heartImg} alt={altText} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ExploreMenu;
