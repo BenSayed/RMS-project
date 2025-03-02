a02f13566b2564f61f67cbe17b3a53ad3e36cfcd
Ben Sayed <Ahmed8Sayed1010@gmail.com>
2025-03-02 09:26:29 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-import Home from "./page-index/Home/Home";
-import WaitingPage from "./Handel Page/Waiting page/waitingPage";
-import PageErorr500 from "./Page Erorr/Page500/pageErorr500";
-import Erorr404 from "./Page Erorr/page404/Erorr404";
-import Countact from "./page-index/Contaut us/ContautUs";
+import FOOTER from "./Component/FOOTER/FOOTER";
 import Header from "./Component/Header/Header";
 import Login from "./LoginPage/login/login";
-import SignUp from "./LoginPage/Sign up/SignUp.jsx";
-import ExploreMenu from "./page-index/MenuPage/ExploreMenu";
-// import FOOTER from "./Component/FOOTER/FOOTER.jsx";
-import Reservation from "./page-index/Reservation/reservation";
-import MenuComponent from "./page-index/MenuPage/Explore Menu/explor";
-import SalesPages from "./Sales pages/pageSales1/SalesPages";
-import AboutPage from "./page-index/About/aboutPage";
-import ChefuiPage from "./page-index/chiefUiPage/chefuiPage";
-import DeliveryLogin from "./page-index/Delivery pages/DeliveryLogin";
-import DeliveryHome from "./page-index/Delivery pages/Delivery Home/DeliveryHome";
-import DeliverypageDeteils from "./page-index/Delivery pages/DeliverypageDeteils/DeliverypageDeteils";
-import ForgetPassword from "./LoginPage/ForgetPassword/ForgetPassword";
-import EmailConfirmation from "./LoginPage/ConfirmEmail/ConfirmEmail";
-import PasswordRecovery from "./LoginPage/PasswordRecovery/PasswordRecovery";
-import DeliveryHistory from "./page-index/Delivery pages/DeliveryHistory/DeliveryHistory";
-import Login1 from "./LoginPage/login/login1";
+
+ 
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       {/* <Countact /> */}
       {/* <WaitingPage/> */}
       {/* <PageErorr500/> */}
