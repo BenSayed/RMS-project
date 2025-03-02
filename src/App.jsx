358ff5960fde03416f5227c8b2288683f1c7918f
Ben Sayed <Ahmed8Sayed1010@gmail.com>
2025-03-02 09:58:34 +0200

    fixed typo issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import FOOTER from "./Component/Footer/FOOTER";
+import Footer from "./Component/Footer/Footer";
 import Header from "./Component/Header/Header";
 import Login from "./LoginPage/login/login";
 
@@ -34,7 +34,7 @@ function App() {
       {/* <ExploreMenu /> */}
       {/* <ChefuiPage/> */}
       {/* <DeliveryLogin/> */}
-      <FOOTER/>
+      <Footer/>
     </>
   );
 }
